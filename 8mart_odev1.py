3b6788e98a3f8781724d46bfebd27ae84ae99c8c
barelik35 <barelik35@gmail.com>
2023-03-11 09:02:47 +0300

    Create 8mart_odev1.py

---
+++ 8mart_odev1.py
@@ -0,0 +1,22 @@
+# 1-Python'da Veri Tiplerini araştırınız, her bir veri tipi için kendi cümlelerinizle açıklamalar yazınız.
+Pythonda veri tipi çeşitleri aşağıdaki şekildedir:
+ Metin verilerini barındırır. = String(str)
+ Sayısal veri tiplerini barındırır. = int, float, complex
+ Listeleme, gruplama ve sıralama tiplerinin barındırır. = list, tuple, range  
+
+# 2-Kodlama.io sitesinde değişken olarak kullanıldığını düşündüğünüz verileri, veri tipleriyle birlikte örneklendiriniz.
+Sitede metinsel ifadeler (str) veri tipinde tutuluyordur.
+Kurslardaki ilerlemeler ise (int) veri tipinde tutuluyordur.
+Kullanıcıya ana sayfada kaç tane kursun gösterileceğini sınırlamak için (range) kullanıyordur.
+
+# 3-Kodlama.io sitesinde şart blokları kullanıldığını düşündüğünüz kısımları örneklendiriniz ve Python dilinde bu örnekleri koda dökünüz.
+kategori = ("Tümü","Programlama")
+egitmen = ("Tümü","Engin Demiroğ", "Halit Enes Kalayci")
+kurslar = ("kurs1","kurs2","kurs3","kurs4","kurs5","kurs6","kurs7","kurs8")
+
+if kategori == "Tümü" :
+  print(kurslar)
+elif egitmen == "Engin Demiroğ" :
+  print(kurslar(range(0,3))
+elif egitmen == "Halit Enes Kalayci" :
+   print(kurslar(range(4,7)
