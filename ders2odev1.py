ab6f0402422e68c0da59828dbd6e3f290c8e0a17
barelik35 <barelik35@gmail.com>
2023-03-14 04:12:14 +0300

    Update ders2odev1.py

--- ders2odev1.py
+++ ders2odev1.py
@@ -13,77 +13,87 @@
 # Ödevde kullanacağınız döngülerin bir tanesi for bir tanesi while döngüsü olması istenmektedir
 
 ogrenciler = []
+
+
 #ogrenci eklemek için komut
 def ogrenciEkle(count):
-    for i in range(count):
-        ad=input("Lütfen eklemek istediğiniz öğrenci adını giriniz: ")
-        soyad=input("Lütfen eklemek istediğiniz öğrenci soyadını giriniz: ")
-        adsoyad=ad+soyad
-        ogrenciler.append(adsoyad)
-        print("Öğrenci başarıyla kayıt edildi.")
-        anaMenu()
+  for i in range(count):
+    ad = input("Lütfen eklemek istediğiniz öğrenci adını giriniz: ")
+    soyad = input("Lütfen eklemek istediğiniz öğrenci soyadını giriniz: ")
+    adsoyad = ad + " " + soyad
+    ogrenciler.append(adsoyad)
+    print("Öğrenci başarıyla kayıt edildi.")
+    anaMenu()
+
 
 #Öğrenci silmek için komut
 def ogrenciSil(count):
-    for i in range(count):
-        ad=input("Lütfen silmek isteğiniz öğrencinin adını giriniz: ")
-        soyad=input("Lütfen silmek istediğiniz öğrencinin soyadını giriniz: ")
-        adsoyad=ad+soyad
-        ogrenciler.remove(adsoyad)
-        print("Öğrenci başarıyla silindi.")
-        anaMenu()
+  for i in range(count):
+    ad = input("Lütfen silmek isteğiniz öğrencinin adını giriniz: ")
+    soyad = input("Lütfen silmek istediğiniz öğrencinin soyadını giriniz: ")
+    adsoyad = ad + " " + soyad
+    ogrenciler.remove(adsoyad)
+    print("Öğrenci başarıyla silindi.")
+    anaMenu()
 
 
 #Çoklu öğrenci ekleme
 def cokluOgrenciEkle():
-    count=int(input("Kaç adet öğrenci eklemek istediğinizi giriniz: "))
-    
-    for i in range(count):
-        ogrenciEkle(count)
-    anaMenu()
+  count = int(input("Kaç adet öğrenci eklemek istediğinizi giriniz: "))
+
+  for i in range(count):
+    ogrenciEkle(count)
+  anaMenu()
+
 
 #Çoklu öğrenci silme
 def cokluOgrenciSil():
-    count=int(input("Kaç adet öğrenci silmek istediğinizi giriniz: "))
-    
-    for i in range(count):
-        ogrenciSil(count)
-    anaMenu()
+  count = int(input("Kaç adet öğrenci silmek istediğinizi giriniz: "))
+
+  for i in range(count):
+    ogrenciSil(count)
+  anaMenu()
 
 
 #Öğrenci listesini ekrana yazdırmak için
 def ogrenciListele():
-    print("Öğrenci listesi: ")
-    for ad, soyad in ogrenciler:
-        print(f"{ad},{soyad}")
-    anaMenu()
+  print("Öğrenci listesi:  ")
+  print(ogrenciler)
+
+  anaMenu()
+
 
 #Programdan çıkış
 
+
 def cikis():
-    print("Programdan çıkılıyor.")
-    exit()
+  print("Programdan çıkılıyor.")
+  exit()
+
 
 #Kullanıcıya yapmak istediği işlemleri gösteriyoruz.
 
+
 def anaMenu():
-    islem = input("1- Öğrenci Ekle\n2- Öğrenci Sil\n3- Öğrencileri Listele\n4- Çoklu Öprenci Ekle\n5- Çoklu Öğrenci Sil\n6- Çıkış\nLütfen yapmak istediğiniz işlemi giriniz:")
-    if islem == "1":
-        ogrenciEkle(1)
-    elif islem == "2":
-        ogrenciSil(1)
-    elif islem == "3":
-        ogrenciListele()
-    elif islem == "4":
-        cokluOgrenciEkle()
-    elif islem == "5":
-        cokluOgrenciSil()
-    elif islem == "6":
-        cikis()
-    else :
-        print("Hatalı bir giriş yaptınız.\nLütfen tekrar deneyin.")
-        anaMenu()
-    
- 
+  islem = input(
+    "1- Öğrenci Ekle\n2- Öğrenci Sil\n3- Öğrencileri Listele\n4- Çoklu Öprenci Ekle\n5- Çoklu Öğrenci Sil\n6- Çıkış\nLütfen yapmak istediğiniz işlemi giriniz:"
+  )
+  if islem == "1":
+    ogrenciEkle(1)
+  elif islem == "2":
+    ogrenciSil(1)
+  elif islem == "3":
+    ogrenciListele()
+  elif islem == "4":
+    cokluOgrenciEkle()
+  elif islem == "5":
+    cokluOgrenciSil()
+  elif islem == "6":
+    cikis()
+  else:
+    print("Hatalı bir giriş yaptınız.\nLütfen tekrar deneyin.")
+    anaMenu()
+
+
 print("*****Öğrenci Kayıt Sistemine Hoşgeldiniz*****")
 anaMenu()
